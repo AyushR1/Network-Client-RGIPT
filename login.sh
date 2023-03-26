8cb50126cf7b9d5ee53c9cd2c80290ca7ec79d0c
AyushR1 <ayushrathore12501@gmail.com>
2023-03-26 15:19:57 +0530

    Implement the login api
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- login.sh
+++ login.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+
 function f1() {
   echo "Network is not reachable!"
-  # do something here, like restart the network or notify someone
+  # Define the username and password
+  username="admin"
+  password="admin"
+  epochtime=($(date +%s%N) / 1000000)
+  # Define the payload as a URL-encoded string
+  payload="mode=191&username=$username&password=$password&producttype=0&a=$epochtime"
+
+  # Make the API request using cURL with URL-encoded payload data
+  curl --keepalive -X POST -d "$payload" http://192.168.100.1:8090/login.xml 
 }
 
 while true; do
-  if ping -q -c 1 -w 0.5 google.com >/dev/null; then
+  if ping -q -c 1 -W 0.5 google.com >/dev/null; then
     echo "Network is up"
   else
     f1
